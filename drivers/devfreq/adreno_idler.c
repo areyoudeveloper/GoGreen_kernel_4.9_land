6b918652cfc3398cce7e6468c494fb16a2c4ed66
kdrag0n <dragon@khronodragon.com>
2018-11-22 18:04:59 -0800

    adreno_idler: convert to state notifier
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -62,9 +62,6 @@ module_param_named(adreno_idler_active, adreno_idler_active, bool, 0664);
 
 static unsigned int idlecount = 0;
 
-
-
-
 int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 		 unsigned long *freq)
 {
@@ -86,7 +83,10 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 			idlecount--;
 			return 1;
 		}
-
+	} else if (state_suspended) {
+		/* GPU shouldn't be used for much while display is off, so ramp down the frequency */
+		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
+		return 1;
 	} else {
 		idlecount = 0;
 		/* Do not return 1 here and allow rest of the algorithm to
